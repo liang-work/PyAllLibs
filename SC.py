11b89a09fd59e581742761df3a8c3f3f043b6629
liang-work <tianhesl@outlook.com>
2025-04-05 14:39:33 +0800

    Fix the issue

--- SC.py
+++ SC.py
@@ -1,14 +1,13 @@
 import ctypes
 import os
 import sys
-import subprocess
 
 def run_for_admin():
     if os.name == "nt":
         if ctypes.windll.shell32.IsUserAnAdmin():
             return True
         else:
-            subprocess.Popen([ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)], shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE | subprocess.SW_HIDE)
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
             sys.exit()
     else:
         return False
